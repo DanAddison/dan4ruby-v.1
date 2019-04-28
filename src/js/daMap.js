dc23ea357697bacb9a244a47b6c1fba3bb9c0235
Dan Addison <danaddisoncreative@gmail.com>
2019-04-28 16:34:36 +0100

    Map scripts all directly inside map div
    No separate files for this as it's still crapping out on the live site

--- src/js/daMap.js
+++ src/js/daMap.js
@@ -1,48 +1,48 @@
-var map;
-var options = {
-  center: {lat: 51.085597, lng: 1.149856},
-  zoom: 16
-}
+// var map;
+// var options = {
+//   center: {lat: 51.085597, lng: 1.149856},
+//   zoom: 16
+// }
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), options);
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), options);
 
-  var markers = [
-    {
-      coordinates: {lat: 51.085597, lng: 1.149856},
-      // iconImage: 'assets/images/sc-icon.png',
-      content: '<h1>Partaaay!</h1><img src="assets/images/sc-icon.png" alt="">'
-    },
-    {
-      coordinates: {lat: 51.084725, lng: 1.153940},
-      content: '<h1>Choo choo!</h1>' 
-    }
-  ];
+//   var markers = [
+//     {
+//       coordinates: {lat: 51.085597, lng: 1.149856},
+//       // iconImage: 'assets/images/sc-icon.png',
+//       content: '<h1>Partaaay!</h1><img src="assets/images/sc-icon.png" alt="">'
+//     },
+//     {
+//       coordinates: {lat: 51.084725, lng: 1.153940},
+//       content: '<h1>Choo choo!</h1>' 
+//     }
+//   ];
 
-  // loop through markers
-  for( var i = 0; i < markers.length;i++){
-    addMarker(markers[i])
-  };
+//   // loop through markers
+//   for( var i = 0; i < markers.length;i++){
+//     addMarker(markers[i])
+//   };
 
-  function addMarker(props){  
-    var marker = new google.maps.Marker({
-      map: map,
-      position: props.coordinates
-    });
+//   function addMarker(props){  
+//     var marker = new google.maps.Marker({
+//       map: map,
+//       position: props.coordinates
+//     });
 
-    if(props.iconImage){
-      marker.setIcon(props.iconImage);
-    }
+//     if(props.iconImage){
+//       marker.setIcon(props.iconImage);
+//     }
 
-    if(props.content){
-      var infoWindow = new google.maps.InfoWindow({
-        content: props.content,
-        maxWidth: 200
-      });
-    };
+//     if(props.content){
+//       var infoWindow = new google.maps.InfoWindow({
+//         content: props.content,
+//         maxWidth: 200
+//       });
+//     };
     
-    marker.addListener('click', function(){
-      infoWindow.open(map, marker)
-    });
-  }
-}
+//     marker.addListener('click', function(){
+//       infoWindow.open(map, marker)
+//     });
+//   }
+// }
